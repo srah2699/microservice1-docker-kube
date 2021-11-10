bfbcede0fb743bd98ada7b47877fc1c79d493c85
hima <agiru.vasanth@cimpress.com>
2021-11-10 17:30:57 +0530

    i

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,37 @@
 import express,{Application, Request, Response, NextFunction} from 'express';
 import palindrome from './palindrome';
+import { v4 as uuid } from 'uuid';
 import journal from '@ct-shipping/journal'
 import milieu from '@ct-shipping/milieu';
 
 const app: Application = express();
-
-const PORT = 3003;
+const PORT = 3004;
 app.use(express.json());
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  const traceID = req.headers.traceid;
-  milieu.set('x-trace-id', traceID);
+  const id = req.headers['x-correlation-id'];
+  const traceID = id || uuid();
+  milieu.set('x-correlation-id', traceID);
   milieu.run(() => {
-    journal.registerContext('x-trace-id');
-    journal.info(`Trace ID was injected: ${traceID}`);
+    journal.registerContext('x-correlation-id');
+    if (id) {
+      journal.info(`Request received with valid x-correlation-id:${id}`);
+    } else {
+      journal.info(
+        `x-correlation-id was missing from the request. Generated new: ${traceID}`
+      );
+    }
     next();
   });
 });
 
-app.post('/d', (req, res) => {
-  const { str } = req.body;
-  const constr = str.concat("D-micro")
-  res.send(constr); 
+app.post('/', (req, res) => {
+  const { value } = req.body;
+  const constr = value.concat("D")
+  journal.info(
+    `Calling 5th microservice with data: ${JSON.stringify({ value: constr })}`
+  );
+  res.send({value : constr}); 
 })
 
 app.get('/test/:string', (req: Request, res: Response) => {
